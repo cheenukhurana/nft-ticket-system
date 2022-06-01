2dc4c8c55ed50d14e822533211acebd37f1c19a7
Cheenu Khurana <cheenu.ck@gmail.com>
2022-06-01 19:04:35 +0530

    name, city inputs for show

--- pages/list-your-show.js
+++ pages/list-your-show.js
@@ -1,10 +1,24 @@
+import { useState } from "react"
 import Layout from "../components/layout"
 
-export default function ListYourShow()
-{
+export default function ListYourShow() {
+
+    const [name, setName] = useState('')
+    const [city, setCity] = useState('')
+
     return (
         <Layout>
-            What do you want to host?
+            <p className="mt-2">Your Name</p>
+            <input className="p-2 border-b" type="text" name="Your Name" placeholder="Full Name" />
+            <p className="mt-2">Event City</p>
+            <select name='Event City' onChange={(e) => { setCity(e.target.value) }}>
+
+
+                <option value="New York" selected>New York</option>
+
+                <option value="Los Angeles">Los Angeles</option>
+
+            </select>
         </Layout>
     )
 }
