55182f0e820d3d2d53b29c142e9cce3e2d31bc1b
Cheenu Khurana <cheenu.ck@gmail.com>
2022-06-01 18:28:56 +0530

    truncate venue to one line

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const events = [
   { id: 1, name: "Excuse Me Brother! By Aakash Gupta", venue: "HK Hall: Ahmedabad", type: "Comedy Shows", price: "499", img: "excuseme-brother.png" },
   { id: 2, name: "Supermoon ft B Praak King of Hearts Tour", venue: "YMCA International Centre: Ahmedabad", type: "Concerts", price: "799", img: "bpraak.png" },
   { id: 3, name: "Jasleen royal live concert", venue: "YMCA International Centre: Ahmedabad", type: "Concerts", price: "499", img: "jasleen-royal.png" },
-  { id: 4, name: "Lost Stories at Hungrito Food Fest 4.0", venue: "Lavish Green By Nirvanaa Corp: Ahmedabad", type: "Music Shows", price: "199", img: "lost-stories.png" },
+  { id: 4, name: "Lost Stories at Hungrito Food Fest 4.0", venue: "Lavish Green By Nirvanaa Corp: Ahmedabad", type: "Music Shows", price: "199", img: "lost-stories.png" }
 ]
 
 const Home: NextPage = () => {
@@ -19,13 +19,13 @@ const Home: NextPage = () => {
 
   return (
     <Layout>
-      <div className="flex flex-1 border">
+      <div className="flex flex-wrap border">
         {events.map((event) => (
-          <div className="mx-4" key={event.id} onClick={() => {handleEventClick(event.id)}}>
+          <div className="px-[1%] basis-1/4 overflow-hidden" key={event.id} onClick={() => {handleEventClick(event.id)}}>
             <img src={`/events/${event.img}`} alt="" className="" />
             <div>
-              <p className="text-lg font-bold overflow-hidden text-ellipsis">{event.name}</p>
-              <p className="text-gray-600">{event.venue}</p>
+              <p className="text-lg font-bold">{event.name}</p>
+              <p className="text-gray-600 truncate">{event.venue}</p>
               <p className="text-sm text-gray-500">{event.type}</p>
               <p className="text-sm text-gray-500">{event.price} XTZ</p>
             </div>
