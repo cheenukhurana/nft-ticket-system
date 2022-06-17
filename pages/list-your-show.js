ddf4513ce6adb2b1f5c1e6a9185d95bf0fc0a157
Cheenu Khurana <cheenu.ck@gmail.com>
2022-06-17 16:51:16 +0530

    list your show details

--- pages/list-your-show.js
+++ pages/list-your-show.js
@@ -5,20 +5,38 @@ export default function ListYourShow() {
 
     const [name, setName] = useState('')
     const [city, setCity] = useState('')
+    const [eventType, setEventType] = useState('')
+    const [numberOfPeople, setNumberOfPeople] = useState('')
+    const [description, setDescription] = useState('')
+    const [date, setDate] = useState('')
 
     return (
         <Layout>
-            <p className="mt-2">Your Name</p>
-            <input className="p-2 border-b" type="text" name="Your Name" placeholder="Full Name" />
+            <p className="mt-2" onChange={(e) => { setName(e.target.value) }}>Your Name</p>
+            <input className="p-2 border-b" type="text" name="Organizer Name" placeholder="Full Name" />
+
             <p className="mt-2">Event City</p>
-            <select name='Event City' onChange={(e) => { setCity(e.target.value) }}>
-
-
+            <select name='Event City' onChange={(e) => { setCity(e.target.value) }} placeholder="Select City">
                 <option value="New York" selected>New York</option>
-
                 <option value="Los Angeles">Los Angeles</option>
-
             </select>
+
+            <p className="mt-2">Event Type</p>
+            <select name='Event Type' onChange={(e) => { setEventType(e.target.value) }} placeholder="Select Event Type">
+                <option value="Exhibtition" selected>Exhibition</option>
+                <option value="Workshop">Workshop</option>
+                <option value="Meetup">Meetup</option>
+            </select>
+
+            <p className="mt-2">Max Number of People to attend</p>
+            <input className="p-2 border-b" type="number" name="Number of People" placeholder="Number of People" />
+
+            <p className="mt-2">Event Date</p>
+            <input className="p-2 border-b" type="date" name="Event Date" placeholder="Select Event Date" />
+
+            <p className="mt-2">Event Short Description</p>
+            <input className="p-2 border-b" type="text" name="Event Description" placeholder="Minimum 50 Characters" />
+
         </Layout>
     )
 }
